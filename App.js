c3e1ef0bde6e158806fbea051ec80c0e13897c4e
shree-varshaa-k <156644283+shree-varshaa-k@users.noreply.github.com>
2024-03-04 21:00:01 -0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 // import logo from './logo.svg';
 import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
+import Spinner from 'react-bootstrap/Spinner';
 import './App.css';
+
 const App = () => {
-  const [todo, setTodo] = useState('')
+  const [todo, setTodo] = useState({
+    id:'',
+    title:'',
+    
+  });
   const [arr, setArr] = useState([])
+  
 
   return (
     <>
@@ -13,9 +20,12 @@ const App = () => {
   type= 'text'
   placeholder='Enter your todo'
   value= {todo.title}
-  onChange={(e) => setTodo(e.target.value)}
+  onChange={(e) => setTodo({id: Math.random(),title:e.target.value})}
+  
   />
-  <Button variant="success" className= "box2" onClick= {() => setArr([...arr, todo])}>Add todo</Button>
+  <Button variant="success" className= "box2" onClick= {() => setArr([...arr, todo,localStorage.setItem('todo.title,todo.id')])}>Add todo</Button>
+  <Spinner animation="border" variant="primary" />
+  
   <div >
 {arr.map((item) => (
   <p key={item.id}>{item.title}</p>
@@ -26,4 +36,6 @@ const App = () => {
    </>
     )
 }
-export default App
+
+
+export default App;
